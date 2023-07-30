aa42aa88a0aa20d5864b937de2d8269f8c06ca64
yanst <stepuro.yan@gmail.com>
2023-07-30 10:17:42 +0300

    добавлен вывод заметки по id, вывод заметок на определенную дату

--- Notebook.py
+++ Notebook.py
@@ -11,7 +11,7 @@
 import json
 import datetime
 
-filename = "notes.txt"
+filename = "notebook.json"
 
 # def start():
 #     data = {}
@@ -37,8 +37,8 @@ def add_note(filename):
         'id': calc_id(filename),
         'head': input('Название заметки: '),
         'note': input('Текст заметки: '),
-        'created': f"{current_datetime.date()}" + ' ' + f"{current_datetime.hour}" + ":" + f"{current_datetime.minute}",
-        'last_modified': f"{current_datetime.date()}" + ' ' + f"{current_datetime.hour}" + ":" + f"{current_datetime.minute}",
+        'created': datetime.datetime.strftime(current_datetime, '%Y.%m.%d %H:%M:%S'),
+        'last_modified': datetime.datetime.strftime(current_datetime, '%Y.%m.%d %H:%M:%S')
     })
     write_file(filename, data)
 
@@ -53,7 +53,7 @@ def calc_id(filename):
     return next_id
 
 def change_note(filename):
-    show_all_notes(filename)
+    get_all_notes(filename)
     input_id = int(input('Введите id заметки для редактирования: '))
     new_head = input('Название заметки: ')
     new_note = input('Текст заметки: ')
@@ -65,11 +65,11 @@ def change_note(filename):
         if input_id == i['id']:
             i['head'] = new_head
             i['note'] = new_note
-            i['last_modified'] = f"{current_datetime.date()}" + ' ' + f"{current_datetime.hour}" + ":" + f"{current_datetime.minute}"
+            i['last_modified'] = datetime.datetime.strftime(current_datetime, '%Y.%m.%d %H:%M:%S')
     write_file(filename, data)
 
 def delete_note(filename):
-    show_all_notes(filename)
+    get_all_notes(filename)
     input_id = int(input('Введите id заметки, которую нужно удалить: '))
     data = read_file(filename)
 
@@ -80,7 +80,7 @@ def delete_note(filename):
         index_count += 1
     write_file(filename, data)
 
-def show_all_notes(filename):
+def get_all_notes(filename):
     data = read_file(filename)
     for i in data['notes']:
         print('Id: ' + str(i['id']))
@@ -90,10 +90,50 @@ def show_all_notes(filename):
         print('Last modified: ' + i['last_modified'])
         print('')
 
+def get_note_by_id(filename):
+    input_id = int(input('Введите id заметки, которую нужно вывести: '))
+    data = read_file(filename)
+    for i in data['notes']:
+        if input_id == i['id']:
+            print('Id: ' + str(i['id']))
+            print('Head: ' + i['head'])
+            print('Note: ' + i['note'])
+            print('Created: ' + i['created'])
+            print('Last modified: ' + i['last_modified'])
+            print('')
+
+def get_notes_by_date_created(filename):
+    print('Введите дату, на которую нужно вывести заметки.')
+    input_year = int(input('Год: '))
+    input_month = int(input('Месяц: '))
+    input_day = int(input('День: '))
+    input_date = datetime.date(input_year, input_month, input_day)
+
+    data = read_file(filename)
+
+    print('\n')
+    print('Заметки на дату создания', f"{input_date}")
+          
+    for i in data['notes']:
+        date_created = datetime.datetime.strptime(i['created'], '%Y.%m.%d %H:%M:%S')
+        if date_created.date() == input_date: 
+            print('Id: ' + str(i['id']))
+            print('Head: ' + i['head'])
+            print('Note: ' + i['note'])
+            print('Created: ' + i['created'])
+            print('Last modified: ' + i['last_modified'])
+            print('')
+        
+
 # add_note(filename)
 # change_note(filename)
 # delete_note(filename)
 # show_all_notes(filename)
+# add_note(filename)
+# get_all_notes(filename)
+# change_note(filename)
+# get_all_notes(filename)
+# get_notes_by_date_created(filename)
 
 def menu():
     print('Добро пожаловать в заметки!')
@@ -102,12 +142,15 @@ def menu():
         print('1 - Показать все заметки')
         print('2 - Добавить заметку')
         print('3 - Изменить заметку')
-        print('4 - Удалить заметку')
+        print('4 - Открыть заметку по id')
+        print('5 - Открыть все заметки по дате')
+        print('6 - Удалить заметку')
+        
         print('0 - Закрыть заметки')
         user_operation = int(input('Введите пункт меню: '))
 
         if user_operation == 1:
-            show_all_notes(filename)
+            get_all_notes(filename)
             continue
         elif user_operation == 2: 
             add_note(filename)
@@ -116,6 +159,12 @@ def menu():
             change_note(filename)
             continue
         elif user_operation == 4:
+            get_note_by_id(filename)
+            continue
+        elif user_operation == 5:
+            get_notes_by_date_created(filename)
+            continue
+        elif user_operation == 6:
             delete_note(filename)
             continue
         elif user_operation == 0:
